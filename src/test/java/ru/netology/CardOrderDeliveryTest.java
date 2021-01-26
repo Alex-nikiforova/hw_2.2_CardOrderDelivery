d7bd605eaa82f52f77e1bdcfca28487f3e7c6927
Alexandra Nikiforova <sunny.alej@gmail.com>
2021-01-26 14:36:15 +0300

    fixed test should SuccessfullyWithAutocomplete

--- src/test/java/ru/netology/CardOrderDeliveryTest.java
+++ src/test/java/ru/netology/CardOrderDeliveryTest.java
@@ -1,43 +1,40 @@
 package ru.netology;
 
-import com.codeborne.selenide.ClickOptions;
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
+import javax.annotation.Nonnull;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardOrderDeliveryTest {
 
-    private ClickOptions equalsMonths() {
+    public void selectPlusSevenDaysDate() {
 
-        DateTimeFormatter formatterDays = DateTimeFormatter.ofPattern("dd");
+        DateTimeFormatter formatterDays = DateTimeFormatter.ofPattern("d");
         String meetingDate = LocalDate.now().plusDays(7).format(formatterDays);
 
-        DateTimeFormatter formatterMonths = DateTimeFormatter.ofPattern("MM");
+        DateTimeFormatter formatterMonths = DateTimeFormatter.ofPattern("M");
         int month1 = Integer.parseInt(LocalDate.now().format(formatterMonths));
         int month2 = Integer.parseInt(LocalDate.now().plusDays(7).format(formatterMonths));
 
         if (month2 == month1) {
             $("[type='button']").click();
-            $$(".calendar__day").shouldHave(CollectionCondition.itemWithText(meetingDate));
+            $$(".calendar__day").find(text(meetingDate)).click();
         }
         if (month2 > month1) {
             $("[type='button']").click();
             $("[data-step='1']").click();
-            $$(".calendar__day").shouldHave(CollectionCondition.itemWithText(meetingDate));
+            $$(".calendar__day").find(text(meetingDate)).click();
         }
-        return null;
     }
 
     @BeforeEach
@@ -65,13 +62,19 @@ public class CardOrderDeliveryTest {
     @Test
     public void shouldSuccessfullyWithAutocomplete() {
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String meetingDate = LocalDate.now().plusDays(7).format(formatter);
+
         $("[placeholder='Город']").setValue("Ха");
         $(byText("Хабаровск")).click();
-        $("[placeholder='Дата встречи']").click(Objects.requireNonNull(equalsMonths()));
+        selectPlusSevenDaysDate();
         $("[name='name']").setValue("Курбатов Владислав");
         $("[name='phone']").setValue("+79066786545");
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+
     }
 }
